e2865e7313262b82864c66d9d82109ce68af571c
Monkey0-9 <prakashpraveen239@gmail.com>
2026-05-09 05:58:25 +0530

    Cleanup Trunk: Remove local logs, tools, and temp files from repository

--- .trunk/tools/golangci-lint.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo There was an error while installing golangci-lint2. See .trunk/out/4RdDn.yaml for details.
-exit /b 1;
--- .trunk/tools/shellcheck.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo There was an error while installing shellcheck. See .trunk/out/vInTI.yaml for details.
-exit /b 1;
